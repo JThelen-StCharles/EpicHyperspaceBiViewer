a6d428fe963762ef333eb8cbc551c70dbf800b0f
John Thelen <jgthelen@scmc.org>
2021-03-05 09:57:00 -0800

    Add project files.

---
+++ HyperspaceBiViewer/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.PowerBI.Api.V2;
+using Microsoft.PowerBI.Api.V2.Models;
+using Microsoft.Rest;
+using PowerBIEmbedded_AppOwnsData.Models;
+using PowerBIEmbedded_AppOwnsData.Services;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace PowerBIEmbedded_AppOwnsData.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly IEmbedService m_embedService;
+
+		public HomeController()
+		{
+			m_embedService = new EmbedService();
+		}
+
+		public ActionResult Index()
+		{
+			var result = new IndexConfig();
+			var assembly = Assembly.GetExecutingAssembly().GetReferencedAssemblies().Where(n => n.Name.Equals("Microsoft.PowerBI.Api")).FirstOrDefault();
+			if (assembly != null)
+			{
+				result.DotNETSDK = assembly.Version.ToString(3);
+			}
+			return View(result);
+		}
+
+		public async Task<ActionResult> EmbedReport(string id, string id1)
+		{
+			// --- If we don't have a workspace ID and a Report ID then we need to go back to the index.
+			if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(id1))
+			{
+				return RedirectToAction("Index");
+			}
+
+			var embedResult = await m_embedService.EmbedReport(id, id1);
+			return View(m_embedService.EmbedConfig);
+		}
+	}
+}
---
+++ HyperspaceBiViewer/Services/IEmbedService.cs
@@ -0,0 +1,18 @@
+﻿using PowerBIEmbedded_AppOwnsData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace PowerBIEmbedded_AppOwnsData.Services
+{
+    public interface IEmbedService
+    {
+        EmbedConfig EmbedConfig { get; }
+        TileEmbedConfig TileEmbedConfig { get; }
+
+        Task<bool> EmbedReport(string workspaceId, string reportId);
+    }
+}
